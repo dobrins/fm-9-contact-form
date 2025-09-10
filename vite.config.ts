218ab053812c90ea10226bce298a4d16fc2fb649
dostoyanov <dstoyanov@vedamo.com>
2025-09-11 01:12:28 +0300

    mend

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/fm-9-contact-form",
+  // base: process.env.VITE_BASE_PATH || "/fm-11-multi-step-form",
+  base: process.env.VERCEL ? "/" : "/fm-11-multi-step-form/",
 });
